1225166f1ce7a916800c77a11770498f72718127
Stefan Hajnoczi <stefanha@gmail.com>
2010-10-02 15:44:24 +0100

    Add chunk manager for looking up chunks
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

--- chunk.go
+++ chunk.go
@@ -3,6 +3,8 @@ package main
 import (
 	"io"
 	"os"
+	"log"
+	"path"
 	"nbt"
 )
 
@@ -15,7 +17,7 @@ type Chunk struct {
 	heightMap []byte
 }
 
-func LoadChunk(reader io.Reader) (chunk *Chunk, err os.Error){
+func loadChunk(reader io.Reader) (chunk *Chunk, err os.Error){
 	level, err := nbt.Read(reader)
 	if err != nil {
 		return
@@ -30,5 +32,67 @@ func LoadChunk(reader io.Reader) (chunk *Chunk, err os.Error){
 		blockLight: level.Lookup("/Level/BlockLight").(*nbt.ByteArray).Value,
 		heightMap: level.Lookup("/Level/HeightMap").(*nbt.ByteArray).Value,
 	}
+	return
+}
+
+type ChunkManager struct {
+	worldPath string
+	chunks map[uint64] *Chunk
+}
+
+func NewChunkManager(worldPath string) *ChunkManager {
+	return &ChunkManager{
+		worldPath: worldPath,
+		chunks: make(map[uint64] *Chunk),
+	}
+}
+
+func base36Encode(n int32) (s string) {
+	alphabet := "0123456789abcdefghijklmnopqrstuvwxyz"
+	negative := false
+
+	if n < 0 {
+		n = -n
+		negative = true
+	}
+	if n == 0 {
+		return "0"
+	}
+
+	for n != 0 {
+		i := n % int32(len(alphabet))
+		n /= int32(len(alphabet))
+		s = string(alphabet[i : i+1]) + s
+	}
+	if negative {
+		s = "-" + s
+	}
+	return
+}
+
+func (mgr *ChunkManager) chunkPath(x int32, z int32) string {
+	return path.Join(mgr.worldPath, base36Encode(x&63), base36Encode(z&63),
+		"c."+base36Encode(x)+"."+base36Encode(z)+".dat")
+}
+
+func (mgr *ChunkManager) Get(x int32, z int32) (chunk *Chunk) {
+	key := uint64(x)<<32 | uint64(uint32(z))
+	chunk, ok := mgr.chunks[key]
+	if ok {
+		return
+	}
+
+	file, err := os.Open(mgr.chunkPath(x, z), os.O_RDONLY, 0)
+	if err != nil {
+		log.Exit("ChunkManager.Get: ", err.String())
+	}
+
+	chunk, err = loadChunk(file)
+	file.Close()
+	if err != nil {
+		log.Exit("ChunkManager.loadChunk: ", err.String())
+	}
+
+	mgr.chunks[key] = chunk
 	return
 }
