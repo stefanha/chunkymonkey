43e63d6b20f1e31f7d07b210a2d1a0bd07dea12d
Stefan Hajnoczi <stefanha@gmail.com>
2010-10-03 16:36:28 +0100

    mcs: Add usage information
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

--- mcs.go
+++ mcs.go
@@ -5,6 +5,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		os.Stderr.WriteString("usage: " + os.Args[0] + " <world>\n")
+		os.Exit(1)
+	}
+
 	chunkManager := NewChunkManager(os.Args[1])
 	game := NewGame(chunkManager)
 	game.Serve(":25565")
